630a8d48ce01c66af6cadbeb44dadddbed3cdefb
Musadiq Gujjar <musadiqgujjar582@gmail.com>
2025-05-01 18:24:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+// CHAPTER 01 
+// Chapter 1 (Alerts)
+
+// var firstName = "Musadiq";
+// var lastName = "Gujjar";
+// var email = "musadiqgujjar582@gmail.com";
+// var phoneNumber = "03132089703";
+// var password = "MG-333";
+
+// // Alerts
+// alert(firstName);
+// alert(lastName);
+// alert(email);
+// alert(phoneNumber);
+// alert(password);
+
+
+// alert("I'm learning JavaScript!");
+
+// |End|
+
+// CHAPTER 02 |Start|
+// Chapter 2 (Variables for string)
+
+// var fullName = "Musadiq Gujjar";
+// alert(fullName);
+
+// var MyFav;
+// MyFav = "black";
+// alert(MyFav);
+
+// var teamName = "Code Warriors";
+// alert(teamName);
+
+// var bestMan = "Musadiq";
+// bestMan = "Gujjar";     
+// alert(bestMan);          
+// |End|
+
+// CHAPTER 03 |Start|
+// Chapter 3 (Variables for numbers)
+
+// var caseQty;
+// caseQty = 804;
+// alert(caseQty)
+
+// var num = "8";
+// num = Number(num);
+// var result = num + 5;
+// alert(result)
+
+// var sum;
+// sum = 2 + 2;
+// alert(sum)
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal)
+
+// var num = 10;
+// num = num + 20;
+// alert(num);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
